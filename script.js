1a88d5bc31a2d756e7f59c2cccfb2716920a7ebd
emmariescurrena <emmariescurrena>
2023-09-09 17:58:01 -0300

    Working trough display's input

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
 // HTML buttons
 
-const display = document.getElementById("display");
+const arrCount = document.getElementById("arrCount");
+const prevResult = document.getElementById("prevResult");
 const clearBtn = document.getElementById("clearBtn");
 const deleteBtn = document.getElementById("deleteBtn");
-const button1 = document.getElementById("1");
-const button2 = document.getElementById("2");
-const button3 = document.getElementById("3");
-const button4 = document.getElementById("4");
-const button5 = document.getElementById("5");
-const button6 = document.getElementById("6");
-const button7 = document.getElementById("7");
-const button8 = document.getElementById("8");
-const button9 = document.getElementById("9");
 const plusBtn = document.getElementById("plusBtn");
 const minusBtn = document.getElementById("minusBtn");
 const multiplyBtn = document.getElementById("multiplyBtn");
@@ -19,15 +11,18 @@ const divisionBtn = document.getElementById("divisionBtn");
 const equalBtn = document.getElementById("equalBtn");
 const decimalBtn = document.getElementById("decimalBtn");
 
-// Functions
+// Variables
 
-display.innerText = "Pepe";
+// Functions
 
 function calculate(string) {
   let arr = string.split(" ");
+  let a = +arr[0];
+  let operator = arr[1];
+  let b = +arr[2];
 
-  if (arr[1] == "+") return +arr[0] + +arr[2];
-  else if (arr[1] == "-") return +arr[0] + +arr[2];
-  else if (arr[1] == "*") return +arr[0] + +arr[2];
-  else if (arr[1] == "/") return +arr[0] + +arr[2];
+  if (operator == "+") return a + b;
+  else if (operator == "-") return a - b;
+  else if (operator == "*") return a * b;
+  else if (operator == "/") return a / b;
 }
