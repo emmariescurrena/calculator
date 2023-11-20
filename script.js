11f45ce4e2b8799432109717ced7013ffd610cd7
emmariescurrena <emmariescurrena>
2023-11-20 12:41:30 -0300

    asd

--- script.js
+++ script.js
@@ -2,22 +2,47 @@
 
 const aboveScreen = document.getElementById("aboveScreen");
 const belowScreen = document.getElementById("belowScreen");
-const clearBtn = document.getElementById("clearBtn");
-const deleteBtn = document.getElementById("deleteBtn");
-const plusBtn = document.getElementById("plusBtn");
-const minusBtn = document.getElementById("minusBtn");
-const multiplyBtn = document.getElementById("multiplyBtn");
-const divisionBtn = document.getElementById("divisionBtn");
-const equalBtn = document.getElementById("equalBtn");
-const decimalBtn = document.getElementById("decimalBtn");
+const buttons = document.querySelectorAll(".button");
+
+// Arrays
+
+const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+const operators = ["+", "-", "*", "/"];
+
+// Events functions
+
+writeAboveScreen = (value) => (aboveScreen.innerText = value);
+deleteLastDigit = () =>
+  (belowScreen.innerText = belowScreen.innerText.slice(0, -1));
+clearScreen = (screen) => (screen.innerText = "");
+
+function writeBelowScreen(value) {
+  if (["Backspace", "Delete"].includes(value) && belowScreen.innerText !== "") {
+    deleteLastDigit();
+  } else if (operators.includes(value)) {
+    writeAboveScreen(belowScreen.innerText + " " + value);
+    clearScreen(belowScreen);
+  } else if (["Enter", "="].includes(value)) {
+    let count = `${aboveScreen.innerText} ${belowScreen.innerText}`;
+    clearScreen(aboveScreen);
+    clearScreen(belowScreen);
+    belowScreen.innerText = calculate(count);
+  } else if (numbers.includes(value)) {
+    belowScreen.innerText += value;
+  } else if (value == "Clear") {
+    clearScreen(aboveScreen);
+    clearScreen(belowScreen);
+  }
+}
 
 // Event listeners
 
-window.addEventListener("keydown", writeScreen());
+window.onkeydown = (e) => writeBelowScreen(e.key);
+buttons.forEach(
+  (btn) => (btn.onclick = (e) => writeBelowScreen(e.target.innerText))
+);
 
 // Functions
-
-writeScreen;
 
 function calculate(string) {
   let arr = string.split(" ");
